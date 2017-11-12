a236a1bd3e9b9b91f0a5856b350a1ba9268daaf8
Javier Castillo Calvo <jcastillo@xuitec.com>
2017-11-12 21:11:59 +0100

    fin ejercicio practica 4

--- practica4/main.go
+++ practica4/main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 )
 
 func main() {
 
 	ValorEntero := flag.Int("valor", 0, "Valor Entero de linea de comandos de ejemplo")
 	ValorCadena := flag.String("cadena", "?", "Valor Cadena de linea de comando de ejemplo")
-
+	ValorEspera := flag.Duration("delay", time.Second*1, "Tiempo de espera")
 	flag.Parse()
 
 	fmt.Println("Valor Entero: ", *ValorEntero)
 	fmt.Println("Valor Cadena: ", *ValorCadena)
-	fmt.Printf("%#v\r\n", flag.Args())
+	fmt.Println("Tiempo espera: ", *ValorEspera)
+	fmt.Printf("%#v\r\nwait....", flag.Args())
+	time.Sleep(*ValorEspera)
+	fmt.Println("")
 }
